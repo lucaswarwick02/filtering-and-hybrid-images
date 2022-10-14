9ec39632ef421c9a8ff34f881837b3bef18e15ec
Lucas <lucaswarwick02@gmail.com>
2022-10-14 09:52:57 +0100

    Start zero-padding fix

--- experiments/coursework_functions.py
+++ experiments/coursework_functions.py
@@ -3,27 +3,29 @@ import numpy as np
 
 
 def print_2d_array(array_2d: [[float]], decimal_places=4):
-    print('\n'.join([''.join([('{:.' + str(decimal_places) + 'f} ').format(item) for item in row]) for row in array_2d]))
+    print(
+        '\n'.join([''.join([('{:.' + str(decimal_places) + 'f} ').format(item) for item in row]) for row in array_2d]))
 
 
-def create_guassian_filter(n: int, m: int, sigma=1.0):
+def create_guassian_filter(sigma):
     s = 2.0 * sigma * sigma
+    n = m = int(8 * sigma + 1)
 
-    guassian_filter = [ [0]*n for i in range(m) ]
+    guassian_filter = [[0] * n for i in range(m)]
 
-    sum = 0
+    cum_sum = 0
     x_range = int((n - 1) / 2)
     y_range = int((m - 1) / 2)
 
     for x in range(-x_range, x_range + 1):
         for y in range(-y_range, y_range + 1):
             r = math.sqrt((x * x) + (y * y))
-            guassian_filter[y + y_range][x + x_range] = (math.exp(-(r *r) / 2)) / (math.pi * s)
-            sum += guassian_filter[y + y_range][x + x_range]
+            guassian_filter[y + y_range][x + x_range] = (math.exp(-(r * r) / 2)) / (math.pi * s)
+            cum_sum += guassian_filter[y + y_range][x + x_range]
 
     for x in range(-x_range, x_range + 1):
         for y in range(-y_range, y_range + 1):
-            guassian_filter[y + y_range][x + x_range] /= sum
+            guassian_filter[y + y_range][x + x_range] /= cum_sum
 
     return guassian_filter
 
@@ -33,7 +35,7 @@ def apply_kernel(image: [[float]], kernel: [[float]]):
     n, m = len(kernel), len(kernel[0])
     new_x, new_y = x - n + 1, y - m + 1
 
-    new_image = [[0]*new_y for i in range(new_x)]
+    new_image = [[0] * new_y for i in range(new_x)]
 
     for row_offset in range(0, new_x):
         for column_offset in range(0, new_y):
@@ -44,3 +46,18 @@ def apply_kernel(image: [[float]], kernel: [[float]]):
             new_image[row_offset][column_offset] = sum
 
     return new_image
+
+
+def red_text(text: str):
+    return "\x1b[31m\"" + text + "\"\x1b[0m"
+
+
+def green_text(text: str):
+    return "\x1b[32m\"" + text + "\"\x1b[0m"
+
+
+def test_text(test_name: str, bool_exp: bool):
+    if bool_exp:
+        return green_text(f'{test_name}: PASSED')
+    else:
+        return red_text(f'{test_name}: FAILED')
